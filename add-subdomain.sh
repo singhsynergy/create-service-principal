863880f66eaebd9ca52aab30669b984fb7cd7ff0
Jaswinder Singh <59972508+jassi-devops@users.noreply.github.com>
2023-11-06 16:07:45 +0530

    Update add-subdomain.sh

--- add-subdomain.sh
+++ add-subdomain.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-RG=rgrp-auora
-DOMAIN=ipwatch.net
+RG=<Azure RG>
+## Domain name like abc.com
+DOMAIN=<fully qualified Domain Name>
 az login \
   --service-principal \
-   --username "498e8a56-1027-4137-a182-75813f6ade33" \
-   --password "919112ed-f1bd-4cb8-bc5d-04054d70b2f2" \
-   --tenant "3efccf29-1384-4f0a-ad1a-70ba4d05bc80" >> /dev/null
+   --username "${spn_app_id}" \
+   --password "<your_service_principal_password>" \
+   --tenant "${tenant_id}" >> /dev/null
 
-read -r -p "Enter the Sub-domain name: " SUB_DOMAIN
-read -r -p "Enter the IP_ADDRESS: " IP_ADDRESS
+
+read -r -p "Enter only Sub-domain name. for example, if you want to create a subdomain test.abc.com then just type test: " SUB_DOMAIN
+read -r -p "Enter the IP_ADDRESS on which you want to bind the sub-domain: " IP_ADDRESS
 az network dns record-set a add-record -g $RG -z $DOMAIN -n $SUB_DOMAIN -a $IP_ADDRESS >> /dev/null
