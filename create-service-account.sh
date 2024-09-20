5566decd864c41cb9ad387ce7b01337b092583eb
Jaswinder Singh <59972508+jassi-devops@users.noreply.github.com>
2024-09-20 13:47:24 +0530

    Update create-service-account.sh

--- create-service-account.sh
+++ create-service-account.sh
@@ -6,7 +6,7 @@ app_display_name="$app_display_name"
 subscription_id="$subscription_id"
 az login
 az account set -s "${subscription_id}"
-az ad app create --display-name "${app_display_name}"
+az ad app create --display-name "${app_display_name}" > Credentials.txt
 app_obj_id=$( \
   az ad app list \
     --display-name "${app_display_name}" \
@@ -20,7 +20,7 @@ spn_app_id=$( \
     --query [].appId \
     --output tsv
 )
-az ad sp credential reset --name "${spn_app_id}"
+az ad sp credential reset --id "${spn_app_id}"
 spn_obj_id=$( \
   az ad sp list \
     --display-name "${app_display_name}" \
@@ -39,3 +39,4 @@ az role assignment create \
   --role "Contributor" \
   --scope "/subscriptions/${subscription_id}"
 
+cat Credentials.txt
